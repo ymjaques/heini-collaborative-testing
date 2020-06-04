e30dc66739e266903d73992cd1ffa1b44a628300
ymjaques <ymjaques@users.noreply.github.com>
2020-06-04 11:41:40 +0300

    restore function subtract; fixes #1

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ def test_add():
     assert add('space', 'ship') == 'spaceship'
 
 def subtract(a, b):
-    return a + b  # <--- fix this in step 8
+    return a - b  # <--- fix this in step 8
 
 
 # uncomment the following test in step 5
